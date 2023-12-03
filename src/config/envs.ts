da3c334ef4832c230abf61befd4c6e23ca9b3e58
luisalcalam <luisalcala.ev@gmail.com>
2023-12-03 11:20:12 -0600

    Restore database name

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,5 +4,6 @@ import { get } from 'env-var';
 export const envs = {
   PORT: get('PORT').required().asPortNumber(),
   POSTGRES_URL: get('POSTGRES_URL').required().asString(),
+  POSTGRES_DB: get('POSTGRES_DB').required().asString(),
   JWT_SEED: get('JWT_SEED').required().asString(),
 };
